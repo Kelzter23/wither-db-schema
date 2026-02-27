e52e305a86465bde3eb9dbeb00fdb77a47cc82a7
BALABAT-20231820 <harkenbalabat@gmail.com>
2026-02-27 14:18:05 +0800

    added inseryion

--- logs_insertion.sql
+++ logs_insertion.sql
@@ -2,8 +2,12 @@
 INSERT INTO user_logs (user_id, action) VALUES
 (1, 'Admin logged in'),
 (1, 'Admin added new marker'),
+(2, 'User Log in'),
 (2, 'User viewed NBSC Spot 2'),
-(3, 'User viewed NBSC Covered Court');
+(2, 'User Log out'),
+(3, 'User Log in'),
+(3, 'User viewed NBSC Covered Court'),
+(2, 'User Log out');
 
 
 SELECT *
